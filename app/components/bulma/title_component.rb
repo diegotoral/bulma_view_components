df1da17aea9091d2582f1a78e9f8e4687e0b8262
Diego Toral <diegotoral@gmail.com>
2023-11-26 15:53:16 -0800

    Create Bulma::TitleComponent

---
+++ app/components/bulma/title_component.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Bulma
+  class TitleComponent < Component
+    def initialize(as: :h1, size: 1)
+      raise ArgumentError, 'invalid value to size' if size > 6
+
+      @as = as
+      @size = size
+    end
+
+    def call
+      content_tag @as, content, class: class_names(
+        'title',
+        "is-#{@size}"
+      )
+    end
+  end
+end
