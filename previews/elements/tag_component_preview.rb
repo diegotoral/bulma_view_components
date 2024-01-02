2186be81cbe78f128a55733e4a654a0202b37b44
Diego Toral <diegotoral@gmail.com>
2024-01-02 12:31:21 -0800

    Create TagComponent previews
    This commit adds a basic preview class demonstrating the usage of
    TagComponent. The new preview class is already in the new un-nested
    strcture for the previews. Existing classes will be converted in later
    commits.

---
+++ previews/elements/tag_component_preview.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Elements::TagComponentPreview < ViewComponent::Preview
+  def default
+    render(Bulma::TagComponent.new) { "Tag label" }
+  end
+
+  def colors; end
+
+  def sizes; end
+end
