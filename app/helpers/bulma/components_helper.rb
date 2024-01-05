eb2290ac362c07390825975937e42499b99f6b6b
Diego Toral <diegotoral@gmail.com>
2024-01-04 16:47:29 -0800

    Create component helpers method

--- app/helpers/bulma/components_helper.rb
+++ app/helpers/bulma/components_helper.rb
@@ -2,5 +2,18 @@
 
 module Bulma
   module ComponentsHelper
+    BULMA_HELPER_COMPONENT_MAP = {
+      block: Bulma::BlockComponent,
+      box: Bulma::BoxComponent,
+      button: Bulma::ButtonComponent,
+      tag: Bulma::TagComponent,
+      title: Bulma::TitleComponent
+    }.freeze
+
+    BULMA_HELPER_COMPONENT_MAP.each do |name, component|
+      define_method :"bulma_#{name}" do |*args, **kwargs, &block|
+        render component.new(*args, **kwargs), &block
+      end
+    end
   end
 end
