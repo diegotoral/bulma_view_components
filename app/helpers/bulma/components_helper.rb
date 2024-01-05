128ae902f538b1da6d083717924417f2cda834ba
Diego Toral <diegotoral@gmail.com>
2024-01-04 16:42:08 -0800

    Autoload app/helpers and load components helper

---
+++ app/helpers/bulma/components_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Bulma
+  module ComponentsHelper
+  end
+end
--- lib/bulma/view_components/engine.rb
+++ lib/bulma/view_components/engine.rb
@@ -6,8 +6,15 @@ module Bulma
       isolate_namespace Bulma::ViewComponents
 
       config.autoload_paths += [
+        root.join("app/helpers"),
         root.join("app/components")
       ]
+
+      initializer "bulma_view_components.helpers" do
+        ActiveSupport.on_load(:action_controller_base) do
+          helper Bulma::ComponentsHelper
+        end
+      end
     end
   end
 end
